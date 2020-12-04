e6e5c047f2eca6e60d86744b674febfcefa13285
Huzaifa Imran <huzaifadsu@gmail.com>
2020-12-04 20:59:34 +0500

    Update autoWriter.py

--- autoWriter.py
+++ autoWriter.py
@@ -1,12 +1,18 @@
-from pynput.keyboard import Key, Controller
-import time
+from pynput.keyboard import Key, Controller, Listener
+import threading
+
+def process_keys(key):
+    if str(key) == "Key.f2":
+        with open('content.txt', 'r') as file:
+            autowriter.type(file.read())
 
 def main():
-    keyboard = Controller()
-    time.sleep(5)
-    with open('content.txt', 'r') as file:
-        text = file.read()
-        keyboard.type(text)
+    global autowriter
+    autowriter = Controller()
+    print('Press f2 to type the text of content.txt file')
+    keylog = Listener(on_press = process_keys)
+    with keylog:
+        keylog.join()
 
         
 if __name__ == '__main__':
